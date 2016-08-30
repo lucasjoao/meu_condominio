436c02161a3eccebaa6e9f4b105ba3ac76230e8c
lucasjoao <lucasjoao.lj@gmail.com>
2016-08-29 21:35:51 -0300

    save user and cond in db from signup

--- src/meu_condominio/models.py
+++ src/meu_condominio/models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class User(models.Model):
 	nome = models.CharField(max_length=100)
 	cpf = models.CharField(max_length=11)
@@ -10,7 +12,14 @@ class User(models.Model):
 	senha_default = models.BooleanField()
 	admin = models.BooleanField()
 
+	def __str__(self):
+		return self.nome
+
+@python_2_unicode_compatible
 class Cond(models.Model):
 	nome_condominio = models.CharField(max_length=100)
 	nro_apartamentos = models.IntegerField()
 	cep = models.CharField(max_length=8)
+
+	def __str__(self):
+		return self.nome_condominio
--- src/meu_condominio/views.py
+++ src/meu_condominio/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 from .forms import LoginForm, SignupForm
+from .models import User, Cond
 
 def index(request):
 	return render(request, 'meu_condominio/index.html')
@@ -23,6 +24,19 @@ def signup(request):
 		form = SignupForm(request.POST)
 
 		if form.is_valid():
+			u = User(nome=request.POST['nome'],
+					 cpf=request.POST['cpf'],
+					 email=request.POST['email'],
+					 senha=request.POST['senha'],
+					 senha_default=False,
+					 admin=True
+					)
+			u.save()
+			c = Cond(nome_condominio=request.POST['nome_condominio'],
+					 nro_apartamentos=int(request.POST['nro_apartamentos']),
+					 cep=request.POST['cep']
+					)
+			c.save()
 			return HttpResponseRedirect(reverse('mc-login'))
 	else:
 		form = SignupForm()
