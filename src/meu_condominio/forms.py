19ac4318b80319ac935675a74954b9714769a28e
lucasjoao <lucasjoao.lj@gmail.com>
2016-09-02 00:16:22 -0300

    fix some indentation problem

--- src/meu_condominio/forms.py
+++ src/meu_condominio/forms.py
@@ -4,41 +4,41 @@ from django import forms
 class LoginForm(forms.Form):
 	nome = forms.CharField(label='Seu nome:',
 		                     widget=forms.TextInput(
-								attrs={'placeholder':'Nome'}
+								         attrs={'placeholder':'Nome'}
 		                     ))
 	password = forms.CharField(label='Sua senha', min_length=8,
-						       widget=forms.PasswordInput(
-								   attrs={'placeholder':'Senha'}
-								))
+						                 widget=forms.PasswordInput(
+								             attrs={'placeholder':'Senha'}
+								             ))
 
 class SignupForm(forms.Form):
 	nome = forms.CharField(label='Seu nome:',
-						   widget=forms.TextInput(
-						   		attrs={'placeholder':'Nome'}
-							))
+  					             widget=forms.TextInput(
+						   		       attrs={'placeholder':'Nome'}
+							           ))
 	email = forms.EmailField(label='Seu e-mail:',
 		                     widget=forms.TextInput(
-		                     	attrs={'placeholder':'E-mail'}
+		                     attrs={'placeholder':'E-mail'}
 		                     ))
 	senha = forms.CharField(label='Sua senha:', min_length=8,
-						   widget=forms.TextInput(
-						   		attrs={'placeholder':'Senha'}
-							))
+          						    widget=forms.TextInput(
+          						   	attrs={'placeholder':'Senha'}
+          							  ))
 	nome_condominio = forms.CharField(label='Nome do condomínio:',
-								      widget=forms.TextInput(
-									  	attrs={'placeholder':'Condomínio'}
-									  ))
+								                    widget=forms.TextInput(
+									  	              attrs={'placeholder':'Condomínio'}
+									                  ))
 	nro_apartamentos = forms.IntegerField(label='Número de apartamentos:',
-										  widget=forms.TextInput(
-										  	attrs={'placeholder':'Quantidade'}
-									  	  ))
+										                    widget=forms.TextInput(
+										  	                attrs={'placeholder':'Quantidade'}
+									  	                  ))
 	cep = forms.CharField(label='CEP:',
-						   widget=forms.TextInput(
-						   		attrs={'placeholder':'CEP'}
-						  ))
+						            widget=forms.TextInput(
+						   		      attrs={'placeholder':'CEP'}
+						            ))
 
 class UpdateForm(forms.Form):
 	password = forms.CharField(label='Sua nova senha', min_length=8,
-						       widget=forms.PasswordInput(
-								   attrs={'placeholder':'Senha'}
-							   ))
+                             widget=forms.PasswordInput(
+                             attrs={'placeholder':'Senha'}
+                             ))
--- src/meu_condominio/views.py
+++ src/meu_condominio/views.py
@@ -9,69 +9,69 @@ from .forms import LoginForm, SignupForm, UpdateForm
 from .models import Cond
 
 def index(request):
-	return render(request, 'meu_condominio/index.html')
+  return render(request, 'meu_condominio/index.html')
 
 def login(request):
-	if request.method == 'POST':
-		form = LoginForm(request.POST)
-		user = authenticate(username=request.POST['nome'],
-		                    password=request.POST['password']
-		                    )
+  if request.method == 'POST':
+    form = LoginForm(request.POST)
+    user = authenticate(username=request.POST['nome'],
+                        password=request.POST['password']
+                        )
 
-		if form.is_valid() and user is not None:
-			if user.is_superuser == False:
-				form = UpdateForm()
-				return HttpResponseRedirect(reverse('mc-update',
-													args=(user.pk,)
-												    ))
-			else:
-				return HttpResponseRedirect(reverse('mc-home',
-					                                args=(user.pk,)
-				                                    ))
-		else:
-			messages.warning(request, 'Nome e/ou senha incorretos!')
-	else:
-		form = LoginForm()
+    if form.is_valid() and user is not None:
+      if user.is_superuser == False:
+        form = UpdateForm()
+        return HttpResponseRedirect(reverse('mc-update',
+                                    args=(user.pk,)
+                                    ))
+      else:
+        return HttpResponseRedirect(reverse('mc-home',
+                                            args=(user.pk,)
+                                            ))
+    else:
+      messages.warning(request, 'Nome e/ou senha incorretos!')
+  else:
+    form = LoginForm()
 
-	return render(request, 'meu_condominio/login.html', {'form' : form})
+  return render(request, 'meu_condominio/login.html', {'form' : form})
 
 def signup(request):
-	if request.method == 'POST':
-		form = SignupForm(request.POST)
+  if request.method == 'POST':
+    form = SignupForm(request.POST)
 
-		if form.is_valid():
-			u = User.objects.create_user(request.POST['nome'],
-										 request.POST['email'],
-										 request.POST['senha']
-										)
-			u.is_superuser = True
-			u.save()
-			c = Cond(nome_condominio=request.POST['nome_condominio'],
-					 nro_apartamentos=int(request.POST['nro_apartamentos']),
-					 cep=request.POST['cep']
-					)
-			c.save()
-			return HttpResponseRedirect(reverse('mc-login'))
-	else:
-		form = SignupForm()
+    if form.is_valid():
+      u = User.objects.create_user(request.POST['nome'],
+                                   request.POST['email'],
+                                   request.POST['senha']
+                                   )
+      u.is_superuser = True
+      u.save()
+      c = Cond(nome_condominio=request.POST['nome_condominio'],
+               nro_apartamentos=int(request.POST['nro_apartamentos']),
+               cep=request.POST['cep']
+               )
+      c.save()
+      return HttpResponseRedirect(reverse('mc-login'))
+  else:
+    form = SignupForm()
 
-	return render(request, 'meu_condominio/signup.html', {'form' : form})
+  return render(request, 'meu_condominio/signup.html', {'form' : form})
 
 def update(request, id):
-	if request.method == 'POST':
-		form = UpdateForm(request.POST)
+  if request.method == 'POST':
+    form = UpdateForm(request.POST)
 
-		if form.is_valid():
-			user = User.objects.get(pk=id)
-			user.set_password(request.POST['password'])
-			user.save()
-			return HttpResponseRedirect(reverse('mc-home',
-					                                args=(user.pk,)
-				                                    ))
-	else:
-		form = UpdateForm()
+    if form.is_valid():
+      user = User.objects.get(pk=id)
+      user.set_password(request.POST['password'])
+      user.save()
+      return HttpResponseRedirect(reverse('mc-home',
+                                          args=(user.pk,)
+                                          ))
+  else:
+    form = UpdateForm()
 
-	return render(request, 'meu_condominio/update.html', {'form' : form})
+  return render(request, 'meu_condominio/update.html', {'form' : form})
 
 def home(request, id):
-	return render(request, 'meu_condominio/home.html')
+  return render(request, 'meu_condominio/home.html')
