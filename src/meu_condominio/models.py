1632cc69b6a962708d55c3b0d0d24c2251fcb0f9
lucasjoao <lucasjoao.lj@gmail.com>
2016-11-28 22:50:34 -0200

    fix db to reservation

--- src/meu_condominio/models.py
+++ src/meu_condominio/models.py
@@ -92,6 +92,7 @@ class Reserva(models.Model):
   condominio = models.ForeignKey(Condominio, on_delete=models.CASCADE)
   dia = models.CharField(max_length=3)
   turno = models.CharField(max_length=5)
+  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
   def __str__(self):
     return self.dia + self.turno
